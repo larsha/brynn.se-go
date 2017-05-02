d68ec6c03a13097aa0195e5d738b93f226705f91
Fredrik Lack <fredrik@brynn.se>
2017-05-02 07:38:11 +0200

    Formatting date with correct reference date for Go

--- app/route/middleware/logrequest/logrequest.go
+++ app/route/middleware/logrequest/logrequest.go
@@ -9,7 +9,7 @@ import (
 // Handler will log the HTTP requests
 func Handler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(time.Now().Format("2017-01-01 13:37:00 PM"), r.RemoteAddr, r.Method, r.URL)
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05 PM"), r.RemoteAddr, r.Method, r.URL)
 		h.ServeHTTP(w, r)
 	})
 }
