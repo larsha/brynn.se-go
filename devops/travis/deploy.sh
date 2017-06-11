752bcc814f4eda4955c3b5c90416f1669eb6b584
Fredrik Lack <fredrik@brynn.se>
2017-06-11 19:40:42 +0200

    Updating gcloud push command

--- devops/travis/deploy.sh
+++ devops/travis/deploy.sh
@@ -3,12 +3,12 @@
 set -e
 
 IMAGE=eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME}
-VERSION_IMAGE=$IMAGE:$TRAVIS_COMMIT
+VERSION_IMAGE=$IMAGE:$COMMIT
 LATEST_IMAGE=$IMAGE:latest
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o .build/main .
 curl -o .build/ca-certificates.crt https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
-docker build --build-arg CACHEBUST=$TRAVIS_COMMIT \
+docker build --build-arg CACHEBUST=$COMMIT \
   -t $VERSION_IMAGE \
   -f Dockerfile.web .
 
@@ -20,7 +20,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
-gcloud docker push $VERSION_IMAGE
+gcloud docker -- push $VERSION_IMAGE
 
 yes | gcloud beta container images add-tag $VERSION_IMAGE $LATEST_IMAGE
 
