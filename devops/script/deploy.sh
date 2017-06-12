b42f41efedcc09d5294ccf89a1c2ad06e4fd804b
Fredrik Lack <fredrik@brynn.se>
2017-06-12 20:15:06 +0200

    Trying out travis job stages

--- devops/script/deploy.sh
+++ devops/script/deploy.sh
@@ -19,21 +19,11 @@ docker build \
   -t $NGINX_IMAGE:latest \
   -f Dockerfile.nginx .
 
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-
-gcloud --quiet config set project $PROJECT_NAME
-gcloud --quiet config set container/cluster $CLUSTER_NAME
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME
-
+# Push to Google Container Registry
 gcloud docker -- push $WEB_IMAGE:$COMMIT
 gcloud docker -- push $WEB_IMAGE:latest
 gcloud docker -- push $NGINX_IMAGE:$COMMIT
 gcloud docker -- push $NGINX_IMAGE:latest
-
-kubectl config view
-kubectl config current-context
 
 # Update web
 kubectl -n ${K8S_NAMESPACE} set image deployment/${K8S_DEPLOYMENT_NAME_WEB} ${K8S_DEPLOYMENT_NAME_WEB}=$WEB_IMAGE:$COMMIT
