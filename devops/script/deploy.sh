355f5c9b7380d85d8aa22731a8f2461994ca950f
Fredrik Lack <fredrik@brynn.se>
2017-06-12 21:25:44 +0200

    Minor comment update

--- devops/script/deploy.sh
+++ devops/script/deploy.sh
@@ -21,7 +21,7 @@ gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 WEB_IMAGE=eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_WEB}
 NGINX_IMAGE=eu.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}/${K8S_DEPLOYMENT_NAME_NGINX}
 
-# Build web
+# Build web image
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o .build/main .
 curl -o .build/ca-certificates.crt https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
 docker build --build-arg CACHEBUST=$COMMIT \
@@ -29,13 +29,13 @@ docker build --build-arg CACHEBUST=$COMMIT \
   -t $WEB_IMAGE:latest \
   -f Dockerfile.web .
 
-# Build nginx
+# Build nginx image
 docker build \
   -t $NGINX_IMAGE:$COMMIT \
   -t $NGINX_IMAGE:latest \
   -f Dockerfile.nginx .
 
-# Push to Google Container Registry
+# Push images
 gcloud docker -- push $WEB_IMAGE:$COMMIT
 gcloud docker -- push $WEB_IMAGE:latest
 gcloud docker -- push $NGINX_IMAGE:$COMMIT
