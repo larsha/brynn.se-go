96b05099f9bb3093fb257fd961ff69f98490fdf8
Fredrik Lack <fredrik@brynn.se>
2017-07-16 21:55:51 +0200

    Saving to home

--- devops/script/deploy.sh
+++ devops/script/deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Download and install Google Cloud SDK
-curl -0 https://storage.googleapis.com/cloud-sdk-release/google-cloud-sdk-159.0.0-linux-x86_64.tar.gz | tar -zx
-./google-cloud-sdk/install.sh
+curl -0 https://storage.googleapis.com/cloud-sdk-release/google-cloud-sdk-159.0.0-linux-x86_64.tar.gz | tar -zx -C ${HOME}
+${HOME}/google-cloud-sdk/install.sh
 source ${HOME}/google-cloud-sdk/path.bash.inc
 
 # Update gcloud components
